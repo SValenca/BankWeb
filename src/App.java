c4c5d6285bbd40137d6acea6e0fe97ae231513fc
Valenca <gabrielsoaresvalenca@gmail.com>
2025-02-25 21:37:08 -0300

    polimorfismo

--- src/App.java
+++ src/App.java
@@ -5,27 +5,34 @@ import domain.model.Pessoa;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-         
-        Agencia agencia = new Agencia();
-        Pessoa pessoa = new Pessoa();
-        Conta conta = new Conta();
-        agencia.nome = "Campo Grande";
-        agencia.numero = "001";
-        agencia.email= "campograndedev@gmail.com";
-        agencia.tel= "+55 (21) 400-28922";
-
-        pessoa.nome = "Joelson da Silva";
-        pessoa.cpf = "123.456.789-10";
-        pessoa.email = "joelsondasilva@gmail.com";
-        pessoa.tel = "+55 (21) 777-88888";
-
-        conta.numero = "100001";
-        conta.agencia = agencia;
-        conta.pessoa = pessoa;
-
-        System.out.println("Agencia:" + conta.agencia.nome);
-        System.out.println("Cliente: " + conta.pessoa.nome);
-        System.out.println("Saldo: " + conta.saldo);
-
-    }
-}
+         try {                                                      //try- tente
+            
+             Agencia agencia = new Agencia();
+             Pessoa pessoa = new Pessoa("Joelson da Silva","joelsondasilva@gmail.com", "+55 (21) 777-88888");
+             Conta conta = new Conta();
+             
+             agencia.nome = "Campo Grande";
+             agencia.numero = "001";
+             agencia.email= "campograndedev@gmail.com";
+             agencia.tel= "+55 (21) 400-28922";
+             
+             //pessoa.setNome("Joelson da Silva"); 
+             pessoa.setCpf("123.456.789-10"); 
+            // pessoa.setEmail("joelsondasilva@gmail.com");
+             //pessoa.setTel("+55 (21) 777-88888");
+             
+             conta.numero = "100001";
+             conta.agencia = agencia;
+             conta.pessoa = pessoa;
+             conta.saldo = 1000;
+             
+             System.out.println("Agencia:" + conta.agencia.nome);
+             System.out.println("Cliente: " + conta.pessoa.getNome());
+             System.out.println("Saldo: " + conta.saldo);
+             
+            } catch (Exception e) {                                  //catch se der erro no try ele trata o erro   
+               System.err.println(e.getLocalizedMessage());          //Exception class que trata erros
+            }
+            }
+        }
+        
--- src/domain/model/Pessoa.java
+++ src/domain/model/Pessoa.java
@@ -2,11 +2,65 @@ package domain.model;
 
 
 public class Pessoa {
-    public String nome;
-    public String cpf;
-    public String cnpj;
-    public String dataNasc;
-    public String email;
-    public String tel;
-   
+    private String nome;
+    private String cpf;
+    private String cnpj;
+    private String dataNasc;
+    private String email;
+    private String tel;
+     
+    public Pessoa(String nome, String email, String tel )throws Exception{
+        this.setNome(nome);     // ou this.nome = nome
+        this.setEmail(email);   // ou this.email = email
+        this.setTel(tel);       // ou this.tel = tel
+    }
+                                                                        //.TRIM ele apaga os espaços do texto (Sempre que manipular String deve usar )
+    public void setNome(String nome)throws Exception{                   //O método .EQUALS é utilizado para comparações.
+        if (nome.trim().equals(""))                            //THROWS (com S no final) é usada em um método para informar ao código de chamada (quem chamou seu método)
+            throw new Exception("nome em branco!");             //que esse método pode gerar uma exceção (dentro dele tem algum código que gera exception) e que 
+        this.nome = nome.trim();                                        //o próprio método não trata (não tem try/catch).  
+            
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public void setCnpj(String cnpj){
+        this.cnpj = cnpj;
+    }
+
+    public void setdataNasc(String dataNasc){
+        this.dataNasc = dataNasc;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setTel(String tel){
+        this.tel = tel;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public String getCpf(){
+        return this.cpf;
+    }
+    
+    public String getCnpj(){
+        return this.cnpj;
+    }
+    
+    public String getDataNasc(){
+        return this.dataNasc;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    public String getTel(){
+        return this.tel;
+    }
+        
 }
